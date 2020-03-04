f99c0f49c68c32716e729232ab82591606a9c441
yuga77 <y-n7c8@i.softbank.jp>
2020-03-04 22:46:33 +0900

    js

---
+++ app/javascript/packs/home/index.js
@@ -0,0 +1,22 @@
+
+
+document.addEventListener('DOMContentLoaded', () => {
+  $('#page, #page1, #page2, #page3, #page4').on('click', function (e) {
+    e.preventDefault();
+    var href = $(this).attr("href");
+    var target = $(href == "#" || href == "" ? 'html' : href);
+    var position = target.offset().top;
+    $("html, body").animate({
+      scrollTop: position
+    }, 300);
+    return false;
+  });
+
+  $('#js-pageTop').on('click', function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 300);
+    return false;
+  });
+
+});
