af85f1ad5588885557407f02e2d92385741301ac
yuga77 <y-n7c8@i.softbank.jp>
2020-03-05 16:32:02 +0900

    test

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,7 +28,6 @@ Rails.application.configure do
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = true
   config.assets.initialize_on_precompile = false
-  config.assets.digest = true
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
